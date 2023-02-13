50f9338a06c4cdd615c7eb460b6a54ddd8160d7c
hlee686 <heso.lee@gmail.com>
2023-02-13 14:10:16 +0900

    Feat: 시터 페이지 interactivity 구현

--- src/modules/sitters.js
+++ src/modules/sitters.js
@@ -1,14 +1,18 @@
+
+
 let headers = {
     "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkVlc3hyc0RsVW5USHkxRmpsdnZUIiwiaWF0IjoxNjc1NjkyNTI4LCJleHAiOjE2NzU3Nzg5MjgsImlzcyI6InRoZXNlY29uQGdtYWlsLmNvbSJ9.8VvD-JwUEt-YJ7LfG8P3vBZd3Zskc_1G7FJemxuJWTo",
     "content-type": "application/json",
     "apikey": "FcKdtJs202301",
-    "username": "KDT4_Team4",
+    "username": "KDT4_Team4_01",
     "masterKey": true
 }
 
-const sitterEl = document.querySelector(".sitterInput")
+const modal = document.getElementById("modal");
+
+const sitterEl = document.querySelector(".search-bar")
 const btn = document.querySelector(".btn")
-const search = document.querySelector(".searchBtn")
+const search = document.querySelector(".search-btn")
 const get = document.querySelector(".getBtn")
 
 let inputValue = ''
@@ -48,7 +52,6 @@ async function seeSitters(){
     const json = await res.json()
     console.log(json)
 }
-
 
 
 async function addSitters(){
@@ -218,49 +221,14 @@ let data = {
         }
      ]
  }
- 
- /*
+
+
  async function getSitters(){
     const res = await fetch("https://asia-northeast3-heropy-api.cloudfunctions.net/api/products",{
         method: "GET",
         headers
     })
-    const items = []
-    const json = await res.json()
-    json.forEach(item => {
-        if(items.includes(item.title)) return
-        if(item.title === inputValue){
-            items.push(item.title)
-            const div = document.createElement("div")
-            div.classList.add(".container")
-            div.style.height = "200px"
-            div.style.width = "800px"
-            div.style.margin = "20px 300px"
-            div.style.border = "3px solid black"
-            div.textContent = item.title
-
-            const priceEl = document.createElement("button")
-            priceEl.textContent = item.price
-            priceEl.style.color = "green"
-            div.append(priceEl)
-
-            const desc = document.createElement("p")
-            desc.textContent = item.description
-            div.append(desc)
-        
-            div.style.backgroundColor = "yellow"
-            document.body.append(div)
-        }
-    })
- }
-*/
-
-async function getSitters(){
-    const res = await fetch("https://asia-northeast3-heropy-api.cloudfunctions.net/api/products",{
-        method: "GET",
-        headers
-    })
-    let pics = ["../asset/sitterImg/1_1.png", "../asset/sitterImg/1_1.png", "../asset/sitterImg/1_1.png", "../sitterImg/asset/1_1.png", "../asset/sitterImg/1_1.png"]
+    let pics = ["../asset/sitterImg/1_2.png", "../asset/sitterImg/1_3.png", "../asset/sitterImg/1_4.png", "../asset/sitterImg/1_6.jpg", "../asset/sitterImg/1_7.jpeg","../asset/sitterImg/1_8.jpeg","../asset/sitterImg/1_9.png","../asset/sitterImg/1_10.jpeg","../asset/sitterImg/1_11.jpeg","../asset/sitterImg/1_12.jpg","../asset/sitterImg/1_13.jpeg","../asset/sitterImg/1_4.jpeg","../asset/sitterImg/1_15.jpg","../asset/sitterImg/1_16.jpg","../asset/sitterImg/1_17.jpeg","../asset/sitterImg/1_18.jpeg","../asset/sitterImg/1_19.jpg","../asset/sitterImg/1_20.jpeg" ]
     const items = []
     const json = await res.json()
     for(let i=0; i<json.length; i++){
@@ -276,16 +244,56 @@ async function getSitters(){
             div.style.borderTop = "1px solid lightgrey"
             div.style.borderBottom = "1px solid lightgrey"
 
+            
+            imgEl = document.createElement("img")
+            imgEl.src = pics[items.length-1]
+            imgEl.style.width = "270px"
+            imgEl.style.height = "120px"
+            imgEl.style.marginLeft = "20px"
+            imgEl.style.marginTop = "42px"
+            div.append(imgEl)
+
+            imgEl.addEventListener("click", function(e){
+                localStorage.setItem("variable", e.target.src);
+                localStorage.setItem("var2", json[i].title)
+                localStorage.setItem("var3", json[i].description)
+                localStorage.setItem("var4", json[i].price)
+                window.location.href = "../pages/detail.html";
+                
+});
+            
+
             const title = document.createElement("h4")
             title.textContent = json[i].title
             title.style.marginLeft = "300px"
-            title.style.marginTop = "55px"
+            title.style.marginTop = "-120px"
             div.append(title)
+
+            const desc = document.createElement("p")
+            desc.style.marginLeft = "300px"
+            desc.style.marginTop = "30px"
+            desc.textContent = json[i].description
+            div.append(desc)
+
+            const purchaseImm = document.createElement("button")
+            purchaseImm.textContent = "바로구매"
+            purchaseImm.style.marginLeft = "700px"
+            purchaseImm.style.marginBottom = "-100px"
+            div.append(purchaseImm)
+            purchaseImm.addEventListener("click", function(){
+                modal.style.display = "block";
+                document.querySelector(".yes").addEventListener("click", function(){
+                    window.location.href = "../pages/login.html"
+                })
+                document.querySelector(".no").addEventListener("click", function(){
+                    modal.style.display = "none"
+                })
+            })
 
             const priceEl = document.createElement("button")
             priceEl.textContent = json[i].price + " KRW"
             priceEl.style.marginLeft = "640px"
-            priceEl.style.marginTop = "50px"
+            priceEl.style.marginTop = "20px"
             priceEl.style.width = "130px"
             priceEl.style.height = "35px"
             priceEl.style.backgroundColor = "green"
@@ -295,19 +303,7 @@ async function getSitters(){
                 window.location.href = "../pages/payment1.html"
             })
 
-            const desc = document.createElement("p")
-            desc.style.marginLeft = "300px"
-            desc.style.marginTop = "-100px"
-            desc.textContent = json[i].description
-            div.append(desc)
 
-            let imgEl = document.createElement("img")
-            imgEl.src = pics[i]
-            imgEl.style.width = "270px"
-            imgEl.style.height = "120px"
-            imgEl.style.marginLeft = "20px"
-            imgEl.style.marginTop = "-102px"
-            div.append(imgEl)
         
             /*
             const deleteBtn = document.createElement("button")
@@ -343,24 +339,12 @@ async function getSitters(){
             */
             
             document.body.append(div)
+
+        
         }
     
 
     }
     
-}
-
-
-async function updateSitters(product, desc, inp){
-    const res = await fetch(`https://asia-northeast3-heropy-api.cloudfunctions.net/api/products/${product.id}`,{
-        method: "PUT",
-        headers,
-        body: JSON.stringify({
-            description: `${inp}`,
-            done: product.done
-        })
-    })
-    desc.textContent = inp
-   
 }
 
