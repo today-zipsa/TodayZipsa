71b87e5c8aabc73d42f29a2d0c9c72e22e5eeaa5
hlee686 <heso.lee@gmail.com>
2023-02-14 13:37:00 +0900

    Feat: 호텔 페이지 기능구현 및 상세페이지와 로그인페이지 연결 구현

--- src/modules/hotel.js
+++ src/modules/hotel.js
@@ -2,7 +2,7 @@ let headers = {
     "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkVlc3hyc0RsVW5USHkxRmpsdnZUIiwiaWF0IjoxNjc1NjkyNTI4LCJleHAiOjE2NzU3Nzg5MjgsImlzcyI6InRoZXNlY29uQGdtYWlsLmNvbSJ9.8VvD-JwUEt-YJ7LfG8P3vBZd3Zskc_1G7FJemxuJWTo",
     "content-type": "application/json",
     "apikey": "FcKdtJs202301",
-    "username": "KDT4_Team4_03",
+    "username": "KDT4_Team4",
     "masterKey": true
 }
 
@@ -14,18 +14,10 @@ const search = document.querySelector(".search-btn")
 const get = document.querySelector(".getBtn")
 
 
-let inputValue = ''
-sitterEl.addEventListener("input", function(e){
-    e.preventDefault()
-    inputValue = e.target.value
-})
 btn.addEventListener("click", function(){
     addSitters()
 })
 
-search.addEventListener("click", function(){
-    getSitters()
-})
 
 get.addEventListener("click", function(){
     seeSitters()
@@ -273,10 +265,12 @@ let data = {
  }
 */
 
+/*
 async function getSitters(){
     const res = await fetch("https://asia-northeast3-heropy-api.cloudfunctions.net/api/products",{
         method: "GET",
-        headers
+        headers,
+        
     })
     let pics = ["../asset/hotelImg/2_1.png", "../asset/hotelImg/2_2.png", "../asset/hotelImg/2_3.png", "../asset/hotelImg/2_4.jpg", "../asset/hotelImg/2_5.png","../asset/hotelImg/2_6.jpg","../asset/hotelImg/2_7.jpg","../asset/hotelImg/2_8.jpg","../asset/hotelImg/2_9.jpg","../asset/hotelImg/2_10.jpg","../asset/hotelImg/2_11.jpg","../asset/hotelImg/2_12.jpg","../asset/hotelImg/2_13.jpg","../asset/hotelImg/2_14.jpg","../asset/hotelImg/2_15.jpg","../asset/hotelImg/2_16.jpg","../asset/hotelImg/2_17.jpg","../asset/hotelImg/2_18.jpg","../asset/hotelImg/2_19.jpg","../asset/hotelImg/2_20.jpg" ]
     const items = []
@@ -387,6 +381,7 @@ async function getSitters(){
             })
             */
             
+            /*
             document.body.append(div)
 
         
@@ -396,4 +391,134 @@ async function getSitters(){
     }
     
 }
+*/
+
+let inputValue = ''
+sitterEl.addEventListener("input", function (e) {
+    e.preventDefault()
+    inputValue = e.target.value
+})
+
+search.addEventListener("click", function () {
+    if(inputValue){
+        getSitters(inputValue, 1)
+    }
+})
+
+
+async function getSitters(searchText, pageNumber){
+    const res = await fetch("https://asia-northeast3-heropy-api.cloudfunctions.net/api/products/search", {
+        method: "POST",
+        headers,
+        body: JSON.stringify({searchText : `${searchText}`})
+
+    })
+
+    const json = await res.json()
+    const result = json.map(item => item)
+    console.log(result)
+
+
+    const items = []
+    let pics = ["asset/hotelImg/2_1.png", "asset/hotelImg/2_2.png", "asset/hotelImg/2_3.png", "asset/hotelImg/2_4.jpg", "asset/hotelImg/2_5.png","asset/hotelImg/2_6.jpg","asset/hotelImg/2_7.jpg","asset/hotelImg/2_8.jpg","asset/hotelImg/2_9.jpg","asset/hotelImg/2_10.jpg","asset/hotelImg/2_11.jpg","asset/hotelImg/2_12.jpg","asset/hotelImg/2_13.jpg","asset/hotelImg/2_14.jpg","asset/hotelImg/2_15.jpg","asset/hotelImg/2_16.jpg","asset/hotelImg/2_17.jpg","asset/hotelImg/2_18.jpg","asset/hotelImg/2_19.jpg","asset/hotelImg/2_20.jpg" ]
+
+    const itemsPerPage = 4
+    const startIndex = (pageNumber - 1) * itemsPerPage
+    const endIndex = startIndex + itemsPerPage
+
+
+    for (let i = startIndex; i < endIndex && i < json.length; i++) {
+        if (items.includes(json[i].title)) return
+        if (json[i].title.includes(searchText)) {
+            items.push(json[i].title)
+            const div = document.createElement("div")
+            div.classList.add("container")
+            div.style.height = "200px"
+            div.style.width = "800px"
+            div.style.margin = "20px 300px"
+            div.style.borderTop = "1px solid lightgrey"
+            div.style.borderBottom = "1px solid lightgrey"
+
+            imgEl = document.createElement("img")
+            imgEl.src = pics[items.length-1]
+            imgEl.style.width = "270px"
+            imgEl.style.height = "120px"
+            imgEl.style.marginLeft = "20px"
+            imgEl.style.marginTop = "42px"
+            div.append(imgEl)
+
+            imgEl.addEventListener("click", function(e){
+                localStorage.setItem("variable", e.target.src);
+                localStorage.setItem("var2", json[i].title)
+                localStorage.setItem("var3", json[i].description)
+                window.location.href = "../pages/detail.html";
+                
+});
+
+            const title = document.createElement("h4")
+            title.classList.add("titleEl")
+            title.textContent = json[i].title
+            title.classList.add("title")
+            title.style.marginLeft = "300px"
+            title.style.marginTop = "-120px"
+            div.append(title)
+
+            const desc = document.createElement("p")
+            desc.style.marginLeft = "300px"
+            desc.style.marginTop = "30px"
+            desc.textContent = json[i].description
+            div.append(desc)
+
+            const purchaseImm = document.createElement("button")
+            purchaseImm.classList.add("baro")
+            purchaseImm.textContent = "바로구매"
+            purchaseImm.style.marginLeft = "700px"
+            purchaseImm.style.marginBottom = "-100px"
+            div.append(purchaseImm)
+            purchaseImm.addEventListener("click", function(){
+                modal.style.display = "block";
+                document.querySelector(".yes").addEventListener("click", function(){
+                    window.location.href = "../pages/login.html"
+                })
+                document.querySelector(".no").addEventListener("click", function(){
+                    modal.style.display = "none"
+                })
+            })
+
+            const priceEl = document.createElement("button")
+            priceEl.classList.add("price")
+            priceEl.textContent = json[i].price + " KRW"
+            priceEl.style.marginLeft = "640px"
+            priceEl.style.marginTop = "20px"
+            priceEl.style.width = "130px"
+            priceEl.style.height = "35px"
+            priceEl.style.backgroundColor = "green"
+            priceEl.style.color = "white"
+            div.append(priceEl)
+            priceEl.addEventListener("click", function(){
+                window.location.href = "../pages/payment1.html"
+            })
+
+
+            document.body.append(div)
+        }
+    }
+    //const pagesCount = Math.ceil(json.length / itemsPerPage)
+    const paginationDiv = document.createElement("div")
+    for (let i = 1; i <= 10; i++) {
+        const buttonEl = document.createElement("button")
+        buttonEl.textContent = i
+        if (i === pageNumber) buttonEl.disabled = true
+        buttonEl.addEventListener("click", function () {
+            document.querySelectorAll(".container").forEach(item => item.remove())
+            getSitters(inputValue, i)
+            paginationDiv.style.position = "fixed"
+        })
+        paginationDiv.style.marginLeft = "600px"
+        paginationDiv.append(buttonEl)
+    }
+
+    document.body.append(paginationDiv)
+}
+
 
