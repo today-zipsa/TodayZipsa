a1053857df0fe08a0aacfc97101b93384451fb45
ronieo <romelzen50@gmail.com>
2023-02-15 23:38:43 +0900

    feat: footer 템플릿 리터럴 분기

---
+++ src/modules/footer.js
@@ -0,0 +1,41 @@
+const footer = /*html*/ `
+  <footer>
+    <div class="footer--top">
+
+      <div class="footer--info">
+        <h1>오늘의집사</h1>
+        <label for="title" class="title">찾아오시는 길</label>
+        <label for="content">서울 특별시 강남구 공주로 17길 NW1건물 8JR층</label>
+      </div>
+
+      <div class="footer--catagories">
+        <label for="title" class="title">카테고리</label>
+        <label for="content">호텔</label>
+        <label for="content">카렌트</label>
+        <label for="content">펫시터</label>
+        <label for="content">펫간식</label>
+      </div>
+
+      <div class="footer--customerCare">
+        <label for="title" class="title">고객페이지</label>
+        <label for="content">마이페이지</label>
+        <label for="content">주문내역</label>
+      </div>
+
+      <div class="footer--pages">
+        <label for="title" class="title">비즈니스</label>
+        <label for="content">오늘의집사 소개</label>
+        <label for="content">오늘의집사와 협업하기</label>
+      </div>
+
+    </div>
+    <div class="footer--bottom">
+      <label for="copyright">©TodayZipsa - All Rights Reserved</label>
+      <div class="social-wrapper">
+        <img src="../asset/facebook.svg" alt="facebook">
+        <img src="../asset/instagram.svg" alt="instagram">
+        <img src="../asset/twitter.svg" alt="twitter">
+      </div>
+    </div>
+  </footer>
+`;
