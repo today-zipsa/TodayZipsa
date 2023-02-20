4f092f139274a6a40d5a9feb4078077c95fe7136
hlee686 <heso.lee@gmail.com>
2023-02-20 16:53:48 +0900

    feat: 메인(호텔) 페이지 레이아웃 및 비즈니스 로직 구현 (#45)
    * feat: 메인 페이지 레이아웃작성
    
    * feat: 메인 호텔 페이지 비즈니스 로직 구현
    
    * feat: 스타일 적용
    
    * feat: 메인 페이지 레잉아웃 수정
    
    * feat: 스타일링 수정
    
    * feat: 메인 페이지 비즈니스 로직 수정
    
    * feat: 카렌트 페이지 레이아웃 구현
    
    * feat: 카렌트 페이지 비즈니스 로직 구현
    
    * feat: 펫시터 페이지 레이아웃 구현
    
    * feat: 펫시터 페이지 비즈니스 로직 구현
    
    * feat: 펫시터 페이지 레이아웃 구현
    
    * feat: 카렌트 페이지 레이아웃 구현
    
    * feat: 호텔 페이지 비즈니스 로직 구현
    
    * feat: 메인 호텔 페이지 레이아웃 구현
    
    * feat: 호텔 페이지 레이아웃 구현
    
    * feat: 메인 호텔 페이지 비즈니스로직 구현
    
    * feat: 메인 페이지 비즈니스 로직 구현
    
    * feat: 불필요한 스타일링 파일 제거
    
    * feat: 메인페이지 비즈니스로직 수정
    
    * feat: 스타일링 수정
    
    * feat: index.html파일 제거

--- src/modules/sitters.js
+++ src/modules/sitters.js
@@ -6,42 +6,38 @@ let headers = {
     "masterKey": true
 }
 
-const sitterEl = document.querySelector(".sitterInput")
+const modal = document.getElementById("modal");
+
+const sitterEl = document.querySelector(".search-bar")
 const btn = document.querySelector(".btn")
-const search = document.querySelector(".searchBtn")
+const search = document.querySelector(".search-btn")
 const get = document.querySelector(".getBtn")
 
-let inputValue = ''
-sitterEl.addEventListener("input", function(e){
-    e.preventDefault()
-    inputValue = e.target.value
-})
-btn.addEventListener("click", function(){
+//const res = await request("PRD08", { searchTags: ["가전"] });
+
+btn.addEventListener("click", function () {
     addSitters()
 })
 
-search.addEventListener("click", function(){
-    getSitters()
-})
 
-get.addEventListener("click", function(){
+get.addEventListener("click", function () {
     seeSitters()
 })
 
 
-async function deleteSitters(todoId){
-    
-    const res = await fetch(`https://asia-northeast3-heropy-api.cloudfunctions.net/api/products/${todoId}`,{
+async function deleteSitters(todoId) {
+
+    const res = await fetch(`https://asia-northeast3-heropy-api.cloudfunctions.net/api/products/${todoId}`, {
         method: "DELETE",
         headers
     })
     const json = await res.json()
     console.log(json)
-    
+
 }
 
-async function seeSitters(){
-    const res = await fetch("https://asia-northeast3-heropy-api.cloudfunctions.net/api/products",{
+async function seeSitters() {
+    const res = await fetch("https://asia-northeast3-heropy-api.cloudfunctions.net/api/products", {
         method: "GET",
         headers
     })
@@ -50,9 +46,8 @@ async function seeSitters(){
 }
 
 
-
-async function addSitters(){
-    for(let i=0; i<data["list"].length; i++){
+async function addSitters() {
+    for (let i = 0; i < data["list"].length; i++) {
         const res = await fetch("https://asia-northeast3-heropy-api.cloudfunctions.net/api/products", {
             method: "POST",
             headers,
@@ -60,232 +55,115 @@ async function addSitters(){
                 title: data["list"][i].title,
                 price: data["list"][i].price,
                 description: data["list"][i].description,
+                tags: data["list"][i].tags,
                 thumbnail: data["list"][i].thumbnailBase64
-           })
-           })
-           const json = await res.json()
-           console.log(json)
-        }
+            })
+        })
+        const json = await res.json()
+        console.log(json)
     }
+}
 
-let data = {
-    "list": [
-         {
-             "title": "강남구 청담동 A",
-             "price": 35000,
-             "description": "강남구 청담동 최고의 시터입니다.",
-             "tags": [],
-             "thumbnailBase64": "../asset/sitterImg/1_1.png"
- 
-         },
-         {
-             "title": "강남구 청담동 B",
-             "price": 35000,
-             "description": "강남구 청담동 최고의 시터입니다.",
-             "tags": [],
-             "thumbnailBase64": "../asset/sitterImg/1_1.png"
-         },
-         {
-             "title": "강남구 청담동 C",
-             "price": 45000,
-             "description": "강남구 청담동 최고의 시터입니다.",
-             "tags": [],
-             "thumbnailBase64": "../asset/sitterImg/1_1.png"
-         },
-         {
- 
-             "title": "강남구 청담동 D",
-             "price": 60000,
-             "description": "강남구 청담동 최고의 시터입니다.",
-             "tags": [],
-             "thumbnailBase64": "../asset/sitterImg/1_1.png"
-         },
-         {
-             "title": "강남구 청담동 E",
-             "price": 30000,
-             "description": "강남구 청담동 최고의 시터입니다.",
-             "tags": [],
-             "thumbnailBase64": "../asset/sitterImg/1_1.png"
-         },
-         {
-            "title": "강남구 압구정동 A",
-            "price": 30000,
-            "description": "강남구 청담동 최고의 시터입니다.",
-            "tags": [],
-            "thumbnailBase64": "../asset/sitterImg/1_1.png"
-        },
-        {
-            "title": "강남구 압구정동 B",
-            "price": 30000,
-            "description": "강남구 청담동 최고의 시터입니다.",
-            "tags": [],
-            "thumbnailBase64": "../asset/sitterImg/1_6.jpg"
-        },
-        {
-            "title": "강남구 압구정동 C",
-            "price": 30000,
-            "description": "강남구 청담동 최고의 시터입니다.",
-            "tags": [],
-            "thumbnailBase64": "../asset/sitterImg/1_7.jpeg"
-        },
-        {
-            "title": "강남구 압구정동 D",
-            "price": 30000,
-            "description": "강남구 청담동 최고의 시터입니다.",
-            "tags": [],
-            "thumbnailBase64": "../asset/sitterImg/1_8.jpeg"
-        },
-        {
-            "title": "강남구 압구정동 E",
-            "price": 30000,
-            "description": "강남구 청담동 최고의 시터입니다.",
-            "tags": [],
-            "thumbnailBase64": "../asset/sitterImg/1_9.png"
-        },
-        {
-            "title": "강남구 신사동 A",
-            "price": 30000,
-            "description": "강남구 청담동 최고의 시터입니다.",
-            "tags": [],
-            "thumbnailBase64": "../asset/sitterImg/1_10.jpeg"
-        },
-        {
-            "title": "강남구 신사동 B",
-            "price": 30000,
-            "description": "강남구 청담동 최고의 시터입니다.",
-            "tags": [],
-            "thumbnailBase64": "../asset/sitterImg/1_11.jpeg"
-        },
-        {
-            "title": "강남구 신사동 C",
-            "price": 30000,
-            "description": "강남구 청담동 최고의 시터입니다.",
-            "tags": [],
-            "thumbnailBase64": "../asset/sitterImg/1_12.jpg"
-        },
-        {
-            "title": "강남구 신사동 D",
-            "price": 30000,
-            "description": "강남구 청담동 최고의 시터입니다.",
-            "tags": [],
-            "thumbnailBase64": "../asset/sitterImg/1_13.jpeg"
-        },{
-            "title": "강남구 신사동 E",
-            "price": 30000,
-            "description": "강남구 청담동 최고의 시터입니다.",
-            "tags": [],
-            "thumbnailBase64": "../asset/sitterImg/1_14.jpeg"
-        },
-        {
-            "title": "강남구 도곡동 A",
-            "price": 30000,
-            "description": "강남구 청담동 최고의 시터입니다.",
-            "tags": [],
-            "thumbnailBase64": "../asset/sitterImg/1_15.jpg"
-        },
-        {
-            "title": "강남구 도곡동 B",
-            "price": 30000,
-            "description": "강남구 청담동 최고의 시터입니다.",
-            "tags": [],
-            "thumbnailBase64": "../asset/sitterImg/1_16.jpg"
-        },{
-            "title": "강남구 도곡동 C",
-            "price": 30000,
-            "description": "강남구 청담동 최고의 시터입니다.",
-            "tags": [],
-            "thumbnailBase64": "../asset/sitterImg/1_17.jpeg"
-        },
-        {
-            "title": "강남구 도곡동 D",
-            "price": 30000,
-            "description": "강남구 청담동 최고의 시터입니다.",
-            "tags": [],
-            "thumbnailBase64": "../asset/sitterImg/1_18.jpeg"
-        },
-        {
-            "title": "강남구 도곡동 E",
-            "price": 30000,
-            "description": "강남구 청담동 최고의 시터입니다.",
-            "tags": [],
-            "thumbnailBase64": "../asset/sitterImg/1_19.jpg"
-        },{
-            "title": "강남구 역삼동 A",
-            "price": 30000,
-            "description": "강남구 청담동 최고의 시터입니다.",
-            "tags": [],
-            "thumbnailBase64": "../asset/sitterImg/1_20.jpeg"
-        }
-     ]
- }
- 
- /*
- async function getSitters(){
-    const res = await fetch("https://asia-northeast3-heropy-api.cloudfunctions.net/api/products",{
-        method: "GET",
-        headers
+
+
+
+let inputValue = ''
+sitterEl.addEventListener("input", function (e) {
+    e.preventDefault()
+    inputValue = e.target.value
+})
+
+search.addEventListener("click", function () {
+    if(inputValue){
+        getSitters(inputValue, 1)
+    }
+})
+
+
+async function getSitters(searchText, pageNumber){
+    const res = await fetch("https://asia-northeast3-heropy-api.cloudfunctions.net/api/products/search", {
+        method: "POST",
+        headers,
+        body: JSON.stringify({searchText : `${searchText}` , searchTags: ['펫시터']})
+
     })
-    const items = []
+
     const json = await res.json()
-    json.forEach(item => {
-        if(items.includes(item.title)) return
-        if(item.title === inputValue){
-            items.push(item.title)
-            const div = document.createElement("div")
-            div.classList.add(".container")
-            div.style.height = "200px"
-            div.style.width = "800px"
-            div.style.margin = "20px 300px"
-            div.style.border = "3px solid black"
-            div.textContent = item.title
+    const result = json.map(item => item)
+    console.log(result)
 
-            const priceEl = document.createElement("button")
-            priceEl.textContent = item.price
-            priceEl.style.color = "green"
-            div.append(priceEl)
 
-            const desc = document.createElement("p")
-            desc.textContent = item.description
-            div.append(desc)
-        
-            div.style.backgroundColor = "yellow"
-            document.body.append(div)
-        }
-    })
- }
-*/
-
-async function getSitters(){
-    const res = await fetch("https://asia-northeast3-heropy-api.cloudfunctions.net/api/products",{
-        method: "GET",
-        headers
-    })
-    let pics = ["../asset/sitterImg/1_1.png", "../asset/sitterImg/1_1.png", "../asset/sitterImg/1_1.png", "../sitterImg/asset/1_1.png", "../asset/sitterImg/1_1.png"]
     const items = []
-    const json = await res.json()
-    for(let i=0; i<json.length; i++){
-        if(items.includes(json[i].title)) return
-        //if(json[i].title === inputValue){
-        if(json[i].title.includes(inputValue)){
+    let pics = ["../asset/sitterImg/1_2.png", "../asset/sitterImg/1_3.png", "../asset/sitterImg/1_4.png", "../asset/sitterImg/1_6.jpg", "../asset/sitterImg/1_7.jpeg", "../asset/sitterImg/1_8.jpeg", "../asset/sitterImg/1_9.png", "../asset/sitterImg/1_10.jpeg", "../asset/sitterImg/1_11.jpeg", "../asset/sitterImg/1_12.jpg", "../asset/sitterImg/1_13.jpeg", "../asset/sitterImg/1_4.jpeg", "../asset/sitterImg/1_15.jpg", "../asset/sitterImg/1_16.jpg", "../asset/sitterImg/1_17.jpeg", "../asset/sitterImg/1_18.jpeg", "../asset/sitterImg/1_19.jpg", "../asset/sitterImg/1_20.jpeg"]
+
+    const itemsPerPage = 2
+    const startIndex = (pageNumber - 1) * itemsPerPage
+    const endIndex = startIndex + itemsPerPage
+
+
+    for (let i = startIndex; i < endIndex && i < json.length; i++) {
+        if (items.includes(json[i].title)) return
+        if (json[i].title.includes(searchText)) {
             items.push(json[i].title)
             const div = document.createElement("div")
-            div.classList.add(".container")
+            div.classList.add("container")
             div.style.height = "200px"
             div.style.width = "800px"
             div.style.margin = "20px 300px"
             div.style.borderTop = "1px solid lightgrey"
             div.style.borderBottom = "1px solid lightgrey"
 
+            imgEl = document.createElement("img")
+            imgEl.src = json[i].thumbnail
+            imgEl.style.width = "270px"
+            imgEl.style.height = "120px"
+            imgEl.style.marginLeft = "20px"
+            imgEl.style.marginTop = "42px"
+            div.append(imgEl)
+
+            imgEl.addEventListener("click", function(e){
+                localStorage.setItem("variable", e.target.src);
+                localStorage.setItem("var2", json[i].title)
+                localStorage.setItem("var3", json[i].description)
+                window.location.href = "../pages/detail.html";
+                
+});
+
             const title = document.createElement("h4")
+            title.classList.add("titleEl")
             title.textContent = json[i].title
+            title.classList.add("title")
             title.style.marginLeft = "300px"
-            title.style.marginTop = "55px"
+            title.style.marginTop = "-120px"
             div.append(title)
 
+            const desc = document.createElement("p")
+            desc.style.marginLeft = "300px"
+            desc.style.marginTop = "30px"
+            desc.textContent = json[i].description
+            div.append(desc)
+
+            const purchaseImm = document.createElement("button")
+            purchaseImm.classList.add("baro")
+            purchaseImm.textContent = "바로구매"
+            purchaseImm.style.marginLeft = "700px"
+            purchaseImm.style.marginBottom = "-100px"
+            div.append(purchaseImm)
+            purchaseImm.addEventListener("click", function(){
+                modal.style.display = "block";
+                document.querySelector(".yes").addEventListener("click", function(){
+                    window.location.href = "../pages/login.html"
+                })
+                document.querySelector(".no").addEventListener("click", function(){
+                    modal.style.display = "none"
+                })
+            })
+
             const priceEl = document.createElement("button")
+            priceEl.classList.add("price")
             priceEl.textContent = json[i].price + " KRW"
             priceEl.style.marginLeft = "640px"
-            priceEl.style.marginTop = "50px"
+            priceEl.style.marginTop = "20px"
             priceEl.style.width = "130px"
             priceEl.style.height = "35px"
             priceEl.style.backgroundColor = "green"
@@ -295,72 +173,34 @@ async function getSitters(){
                 window.location.href = "../pages/payment1.html"
             })
 
-            const desc = document.createElement("p")
-            desc.style.marginLeft = "300px"
-            desc.style.marginTop = "-100px"
-            desc.textContent = json[i].description
-            div.append(desc)
 
-            let imgEl = document.createElement("img")
-            imgEl.src = pics[i]
-            imgEl.style.width = "270px"
-            imgEl.style.height = "120px"
-            imgEl.style.marginLeft = "20px"
-            imgEl.style.marginTop = "-102px"
-            div.append(imgEl)
-        
-            /*
-            const deleteBtn = document.createElement("button")
-            deleteBtn.textContent = "삭제"
-            deleteBtn.addEventListener("click", function(){
-                deleteSitters(json[i].id)
-            })
-            div.append(deleteBtn)
-            */
-        
-            /*
-            const inputEl2 = document.createElement("button")
-            inputEl2.textContent = "수정"
-            div.append(inputEl2)
-            
-
-            let inp = ''
-            inputEl2.addEventListener("click", function(){
-                const inputField = document.createElement("input")
-                div.append(inputField)
-                const buttonEl = document.createElement("button")
-                buttonEl.textContent = "수정완료"
-                div.append(buttonEl)
-
-                inputField.addEventListener("input", function(e){
-                    inp = e.target.value
-                })
-                buttonEl.addEventListener("click", function(){
-                    //desc.textContent = inp
-                    updateSitters(json[i], desc, inp)
-                })
-            })
-            */
-            
             document.body.append(div)
         }
-    
-
     }
-    
-}
-
-
-async function updateSitters(product, desc, inp){
-    const res = await fetch(`https://asia-northeast3-heropy-api.cloudfunctions.net/api/products/${product.id}`,{
-        method: "PUT",
-        headers,
-        body: JSON.stringify({
-            description: `${inp}`,
-            done: product.done
+    //const pagesCount = Math.ceil(json.length / itemsPerPage)
+    const paginationDiv = document.createElement("div")
+    for (let i = 1; i <= 10; i++) {
+        const buttonEl = document.createElement("button")
+        buttonEl.textContent = i
+        if (i === pageNumber) buttonEl.disabled = true
+        buttonEl.addEventListener("click", function () {
+            document.querySelectorAll(".container").forEach(item => item.remove())
+            getSitters(inputValue, i)
+            paginationDiv.style.position = "fixed"
         })
-    })
-    desc.textContent = inp
-   
+        paginationDiv.style.marginLeft = "600px"
+        paginationDiv.append(buttonEl)
+    }
+
+    document.body.append(paginationDiv)
 }
 
+
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
