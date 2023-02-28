03bd907f02139d1858e6cb385afce755e7c461be
gygy7151 <gygy7151@gmail.com>
2023-02-28 17:48:53 +0900

    feat:  공통 헤더 로그아웃 기능 구현 #56 - [x] 공통헤더에 로그아웃 버튼을 클릭했을때 로그아웃 API를 호출한다. - [x] 응답이 완료되면, 로그아웃 버튼과 '반가워요 집사님!'은 사라지고 로그인 버튼이 노출되어야 한다.

--- src/components/templates/header.js
+++ src/components/templates/header.js
@@ -1,3 +1,5 @@
+import request from "../../api/common";
+
 const Header = document.createElement("header");
 const headerWrapper = document.createElement("div");
 const mainLogoBtn = document.createElement("a");
@@ -6,7 +8,8 @@ const searchBtnWrapper = document.createElement("div");
 const searchBar = document.createElement("input");
 const searchBtn = document.createElement("img");
 const welcomeWord = document.createElement("span");
-const logoutBtn = document.createElement("span");
+const btn = document.createElement("a");
+const nameOfBtn = document.createElement("span");
 
 Header.className = "my-header";
 headerWrapper.className = "my-header-wrapper";
@@ -16,7 +19,8 @@ searchBtnWrapper.className = "search-btn-wrapper";
 searchBar.className = "search-bar";
 searchBtn.className = "search-btn";
 welcomeWord.className = "welcome-word";
-logoutBtn.className = "logout";
+btn.className = "btn";
+nameOfBtn.className = "btn-name";
 
 mainLogoBtn.href = "/";
 mainLogoBtnImage.src = require("../../asset/global/main-logo.png");
@@ -26,11 +30,31 @@ searchBar.placeholder = "검색어를 입력해주세요.";
 searchBtn.src = require("../../asset/btnImg/search_btn.png");
 searchBtn.alt = "search-button";
 welcomeWord.innerText = "반가워요, 집사님!";
-logoutBtn.innerText = "로그아웃";
+nameOfBtn.innerText = "로그아웃";
 
 mainLogoBtn.append(mainLogoBtnImage);
 searchBtnWrapper.append(searchBar, searchBtn);
-headerWrapper.append(mainLogoBtn, searchBtnWrapper, welcomeWord, logoutBtn);
+btn.append(nameOfBtn);
+headerWrapper.append(mainLogoBtn, searchBtnWrapper, welcomeWord, btn);
 Header.append(headerWrapper);
 
+btn.addEventListener("click", () => {
+	if (nameOfBtn.innerText === "로그인") {
+		nameOfBtn.innerText = "로그아웃";
+		welcomeWord.innerText = "반가워요, 집사님!";
+	} else {
+		nameOfBtn.innerText = "로그인";
+		welcomeWord.innerText = "";
+		setLogout();
+	}
+});
+
 export default Header;
+// localStorage.setItem("accessToken", 12345); 로컬통신확인용 임의로 집어넣음
+function setLogout() {
+	const token = JSON.parse(localStorage.getItem("accessToken"));
+	if (token) {
+		// console.log("로컬스토리지 토큰 존재여부 체크 완료!");
+		request("MEB04");
+	}
+}
