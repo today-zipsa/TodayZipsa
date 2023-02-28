c65c57504747ac257dc7dc2f2adb1df33f0adb96
gygy7151 <gygy7151@gmail.com>
2023-02-28 20:41:05 +0900

    feat: 공통헤더 로그인/로그아웃 기능 구현 및 마이페이지/어드민 페이지 라우팅 추가
    - [x] 로그인 버튼을 클릭하면 '/login' 로그인페이지로 라우팅 되어야 한다. #114
    - [x] 로그아웃 버튼 클릭 한 후 '/' 메인페이지로 라우팅 되어야 한다. #56
    - [x] '어드민 계정'으로 로그인한 경우, 공통헤더 '반가워요, 집사님!' 버튼 클릭시  '/admin'으로 이동 #111
    - [x] 그외 타 계정으로 로그인한 경우, 공통헤더 '반가워요, 집사님!' 버튼 클릭시  '/my'로 이동 #111

--- src/components/templates/header.js
+++ src/components/templates/header.js
@@ -1,4 +1,5 @@
 import request from "../../api/common";
+const token = JSON.parse(localStorage.getItem("accessToken"));
 
 const Header = document.createElement("header");
 const headerWrapper = document.createElement("div");
@@ -7,9 +8,12 @@ const mainLogoBtnImage = document.createElement("img");
 const searchBtnWrapper = document.createElement("div");
 const searchBar = document.createElement("input");
 const searchBtn = document.createElement("img");
-const welcomeWord = document.createElement("span");
-const btn = document.createElement("a");
-const nameOfBtn = document.createElement("span");
+const welcomeWord = document.createElement("a");
+const loginBtn = document.createElement("a");
+const logoutBtn = document.createElement("a");
+const word = document.createElement("span");
+const nameOfLoginBtn = document.createElement("span");
+const nameOfLogoutBtn = document.createElement("span");
 
 Header.className = "my-header";
 headerWrapper.className = "my-header-wrapper";
@@ -19,8 +23,11 @@ searchBtnWrapper.className = "search-btn-wrapper";
 searchBar.className = "search-bar";
 searchBtn.className = "search-btn";
 welcomeWord.className = "welcome-word";
-btn.className = "btn";
-nameOfBtn.className = "btn-name";
+word.className = "word";
+loginBtn.className = "btn";
+logoutBtn.className = "btn";
+nameOfLoginBtn.className = "btn-name";
+nameOfLogoutBtn.className = "btn-name";
 
 mainLogoBtn.href = "/";
 mainLogoBtnImage.src = require("../../asset/global/main-logo.png");
@@ -29,32 +36,53 @@ searchBar.type = "text";
 searchBar.placeholder = "검색어를 입력해주세요.";
 searchBtn.src = require("../../asset/btnImg/search_btn.png");
 searchBtn.alt = "search-button";
-welcomeWord.innerText = "반가워요, 집사님!";
-nameOfBtn.innerText = "로그아웃";
+welcomeWord.setAttribute("data-navigo", "");
+welcomeWord.href = getNextUrl();
+word.innerText = "반가워요, 집사님!";
+loginBtn.setAttribute("data-navigo", "");
+loginBtn.href = "/login";
+logoutBtn.setAttribute("data-navigo", "");
+logoutBtn.href = "/";
+nameOfLoginBtn.innerText = "로그인";
+nameOfLogoutBtn.innerText = "로그아웃";
+!token ? (logoutBtn.style.display = "none") : (loginBtn.style.display = "none");
 
 mainLogoBtn.append(mainLogoBtnImage);
 searchBtnWrapper.append(searchBar, searchBtn);
-btn.append(nameOfBtn);
-headerWrapper.append(mainLogoBtn, searchBtnWrapper, welcomeWord, btn);
+welcomeWord.append(word);
+loginBtn.append(nameOfLoginBtn);
+logoutBtn.append(nameOfLogoutBtn);
+headerWrapper.append(mainLogoBtn, searchBtnWrapper);
+!token
+	? headerWrapper.append(loginBtn)
+	: headerWrapper.append(welcomeWord, logoutBtn);
 Header.append(headerWrapper);
 
-btn.addEventListener("click", () => {
-	if (nameOfBtn.innerText === "로그인") {
-		nameOfBtn.innerText = "로그아웃";
-		welcomeWord.innerText = "반가워요, 집사님!";
-	} else {
-		nameOfBtn.innerText = "로그인";
-		welcomeWord.innerText = "";
-		setLogout();
-	}
+loginBtn.addEventListener("click", () => {
+	loginBtn.style.display = "none";
+	logoutBtn.style.display = "inline-block";
 });
 
-export default Header;
-// localStorage.setItem("accessToken", 12345); 로컬통신확인용 임의로 집어넣음
+logoutBtn.addEventListener("click", () => {
+	loginBtn.style.display = "inline-block";
+	logoutBtn.style.display = "none";
+	setLogout();
+});
+
 function setLogout() {
-	const token = JSON.parse(localStorage.getItem("accessToken"));
 	if (token) {
-		// console.log("로컬스토리지 토큰 존재여부 체크 완료!");
 		request("MEB04");
+		window.localStorage.clear();
 	}
 }
+
+function getNextUrl() {
+	const id = JSON.parse(localStorage.getItem("id"));
+	if (id === "admin@zipsa.com") {
+		return "/admin";
+	} else {
+		return "/my";
+	}
+}
+
+export default Header;
