873d176e2e79a3372e79b9049fba1587f8609a52
ronieo <romelzen50@gmail.com>
2023-03-01 22:41:27 +0900

    feat: 회원가입 페이지 필수요구사항 기능 구현 #26
    ( #26 )

--- src/modules/join.js
+++ src/modules/join.js
@@ -1,17 +1,17 @@
 import { request } from "../api/common.js";
-import ModalTemplate from "../components/templates/modalTwo";
+import { isUseChecked, isInfoChecked } from "../components/pages/joinPage.js";
+// import ModalTwo from "../components/templates/modalTwo";
 /**
  * 가입 양식 입력란: 닉네임, 계정, 비번, 파일
  */
 
-export default function Join() {
+export default async function Join() {
 	const displayNameEl = document.querySelector(".display-name");
 	const idEl = document.querySelector(".id");
 	const requiredBtnEl = document.querySelector(".validator-btn");
 	const passwordEl = document.querySelector(".password");
 	const passwordCheckEl = document.querySelector(".password-check");
 	const uploadImg = document.querySelector(".upload-img");
-	const uploadInput = document.querySelector("#input_file");
 	const joinBtnEl = document.querySelector(".join-btn");
 
 	// 데이터 관리
@@ -28,6 +28,18 @@ export default function Join() {
 		profileImgBase64: profileImgBase64,
 	};
 
+	idEl.addEventListener("input", (event) => {
+		email = event.target.value.trim();
+	});
+
+	passwordEl.addEventListener("input", (event) => {
+		password = event.target.value.trim();
+	});
+
+	displayNameEl.addEventListener("input", (event) => {
+		displayName = event.target.value.trim();
+	});
+
 	//로그인유무 전역 불린값
 	let signup = false;
 
@@ -36,16 +48,6 @@ export default function Join() {
 		/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
 	// 정규표현식: 비밀번호 영문 대/소문자, 숫자의 2가지 조합, 8~20자
 	const RegexPW = /^(?=.*[a-zA-Z\d])[a-zA-Z\d]{8,}$/;
-
-	displayNameEl.addEventListener("input", (event) => {
-		displayName = event.target.value.trim();
-		// displayName = displayNameEl.value.trim();
-	});
-
-	idEl.addEventListener("input", (event) => {
-		email = event.target.value.trim();
-		// email = idEl.value.trim();
-	});
 
 	/** API : 사용자 목록 조회 */
 	async function getUserList() {
@@ -78,36 +80,35 @@ export default function Join() {
 		// 	alert("동일한 ID가 이미 사용중입니다!");
 		inputCondition();
 	});
+	// 프로필사진: 업로드 버튼 이벤트
+	const inputFileBtn = document.querySelector(".input-file");
+	const inputImg = document.querySelector(".upload-img");
 
-	passwordEl.addEventListener("input", (event) => {
-		password = event.target.value.trim();
-		// password = passwordEl.value.trim();
+	document.querySelector(".upload-btn").addEventListener("click", (e) => {
+		inputFileBtn.click();
+
+		console.log("초록색업로드버튼 눌림");
 	});
 
-	passwordCheckEl.addEventListener("input", (event) => {
-		passwordCheck = event.target.value.trim();
-		// passwordCheck = passwordCheckEl.value.trim();
-	});
-
-	// 프로필사진
-	uploadInput.addEventListener("change", (e) => {
-		const file = e.target.files[0];
+	inputFileBtn.addEventListener("change", (event) => {
+		console.log("되길바래..");
+		const file = event.target.files[0];
+		console.log(file);
 
 		// 파일 사이즈 확인
 		if (file.size > 1024 * 1024) {
 			alert("업로드 가능한 파일의 최대 용량은 1MB입니다.");
-			uploadInput.value = "";
+			mb_thumbnailEl.value = "";
 		} else {
 			const reader = new FileReader();
 			reader.readAsDataURL(file);
 			reader.addEventListener("load", (e) => {
-				console.log(e.target.result);
-				profileImgBase64 = e.target.result;
-				uploadImg = e.target.result;
+				params.profileImgBase64 = e.target.result;
+				inputImg.src = e.target.result;
+				console.log(params);
 			});
 		}
 	});
-
 	// input 유효성 검사
 
 	function inputCondition() {
@@ -123,8 +124,10 @@ export default function Join() {
 		inputName.addEventListener("input", () => {
 			if (inputName.value.length < 3) {
 				wrongName.innerText = "3글자 이상 작성 해 주세요 :)";
+				wrongName.style.color = "#de0404";
 				wrongName.style.display = "block";
 			} else {
+				params.displayName = inputName.value;
 				wrongName.style.display = "none";
 			}
 		});
@@ -134,7 +137,10 @@ export default function Join() {
 				wrongID.style.display = "block";
 				wrongID.style.color = "#de0404";
 				wrongID.innerText = "이메일 양식에 맞춰주세요 :)";
+			} else if (inputID.value === "admin@zipsa.com") {
+				wrongID.innerText = "어드민양식은 가입이 불가합니다 :(";
 			} else {
+				params.email = inputID.value;
 				wrongID.style.display = "none";
 			}
 		});
@@ -146,6 +152,7 @@ export default function Join() {
 				wrongPW.style.color = "#de0404";
 				wrongPW.style.display = "block";
 			} else {
+				params.password = inputPW.value;
 				wrongPW.style.display = "none";
 			}
 		});
@@ -162,22 +169,24 @@ export default function Join() {
 		});
 	}
 
-	function initSignUp() {
-		inputCondition();
-	}
-
 	// 회원가입 버튼
 	joinBtnEl.addEventListener("click", async () => {
+		params.email = idEl.value;
+		params.password = passwordEl.value;
+		params.displayName = displayNameEl.value;
+		console.log("params result>>>>", params);
 		if (joinValidator() === true) {
 			// 회원가입API 호출
 			const res = await request("MEB01", params);
 			console.log("회원가입 성공", res);
 			if (res.accessToken != null) {
 				localStorage.setItem("token", res.accessToken);
+				localStorage.setItem("email", res.email);
+				localStorage.setItem("displayName", res.displayName);
+				localStorage.setItem("profileImg", res.profileImgBase64);
 			}
-			router.navigate("/"); //**** 메인페이지로 보내지도록
+			window.location.href = "/"; //**** 메인페이지로 보내지도록
 		}
-		console.log("joinValidator>>", joinValidator());
 		console.log("회원가입 눌리고있어?>>");
 	});
 
@@ -187,22 +196,17 @@ export default function Join() {
 	function joinValidator() {
 		const Name = document.querySelector(".display-name").value;
 		const PW = document.querySelector(".password").value;
-		const confirmBtn = document.querySelector(".join-btn").value;
-		const cAll = document.querySelector("#cAll"); // 모두체크 하도록 + signup boolean추가
 		if (Name.length < 3) {
 			alert("닉네임을 3글자 이상 입력해주세요 :)");
 			return false;
 		} else if (!signup) {
 			alert("아이디 중복확인을 해주세요 :)");
 			return false;
-		} else if (!cAll.checked) {
+		} else if (isInfoChecked && isUseChecked) {
 			alert("이용 약관을 모두 체크해주세요 :)");
 			return false;
 		} else if (!RegexPW.test(PW)) {
 			alert("비밀번호를 영어,숫자 조합의 \n 8~20자리로 입력 해 주세요 :)");
-			return false;
-		} else if (!(PW === confirmBtn)) {
-			alert("비밀번호와 비밀번호 확인이 일치하지 않습니다 :(");
 			return false;
 		}
 		return true;
